9d252bc0f62b9fdb8806b8a7da4778ecb3a83c39
nivedha <nivedhanssr@gmail.com>
2025-01-14 20:52:07 +0530

    Deleting commented lines

--- tests/scenario1.spec.ts
+++ tests/scenario1.spec.ts
@@ -6,22 +6,12 @@ test.use({
   ignoreHTTPSErrors:true
 })
 test.only("Interact with scrollbar", async({ page })=>{
+  await page.goto('/')
+  await slowExpect(page.getByText('写真から商品を検索してみようカメラやアルバムの写真と似ている商品を検索できます')).toBeVisible()
+  await page.getByTestId('tutorial-overlay').click();
 await Utils.searchProduct(page,'本・雑誌・漫画','本' ,'コンピュータ・IT')
 
-// ///////////////////////////////////////////////////////////////////////////////
-//   // // Get the active descendant of the combobox (the selected option)
-//   // const selectedOption = await page.getByRole('combobox').nth(1).getAttribute('aria-activedescendant');
-//   // // Print the selected option ID
-//   // console.log(selectedOption);
 
-//   // // Now, get the option text content for the selected option
-//   // const selectedOptionText = await page.locator(`#${selectedOption}`).innerText();
-//   // console.log('Selected option text:', selectedOptionText);
-//   /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const selectedOptionText2 = await page.getByRole('combobox').nth(1).locator('option[value="5"]').elementHandles()
-   console.log(await selectedOptionText2.getProperty('selected'))
-  await page.pause()
   await page.getByTestId('カテゴリー').getByLabel('コンピュータ・IT').scrollIntoViewIfNeeded()
   const checkbox = await page.getByTestId('カテゴリー').getByLabel('コンピュータ・IT');
   const isChecked = await checkbox.isChecked();
